b177a4b2a57928d722bac89a98c1fdf5a0f70135
mundy-k <63122459+mundy-k@users.noreply.github.com>
2026-07-22 10:23:39 -0700

    so it's not a readme update, but it moves atlas_overlap out of utils and into atlas.py - bit of testing in calc centroid

---
+++ src/atlas.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+from nibabel import Nifti1Image
+from nilearn.image import resample_to_img
+from utils import *
+
+
+def atlas_overlap(target_atlas:str|Nifti1Image, parcel_num:int, label_atlas:str|Nifti1Image):
+    """
+    Create a dataframe with the labels of regions and number of voxels that overlap across the given parcel number and atlas/parcellation. 
+    
+    Parameters:
+    nifti_file : str or pathlib.Path
+        Path to the labeled parcellation NIfTI file.
+    parcel_num : int
+        Label of the parcel to extract. Must correspond to a valid
+        non-background parcel (background is label 0).
+    atlas_string : str
+        Name of atlas 
+    top_10 : bool
+        If True filters to show only the top 10 corresponding parcels. Otherwise, shows all parcels.
+    
+    Returns a dataframe containing columns:
+        regions = Regional labels for the given atlas/parcellation specified in the atlas_string input, ordered by overlap.
+        intersection_voxels = Number of voxels that overlap across the neurosynth parcel and the given atlas/parcellation specified in the atlas_string input.
+        parcel_overlap = Percentage of voxels in the neurosynth parcellation that overlap with the given atlas/parcellation specified in the atlas_string input.
+
+    """
+
+    if type(label_atlas)==str:
+        atlas = load_nilearn(label_atlas)
+    elif type(label_atlas)==nib.Nifti1Image: 
+        atlas = label_atlas
+
+    _, mask_img = extract_parcel_mask(target_atlas, parcel_num=parcel_num)
+
+    atlas_img = resample_to_img(
+        atlas.maps,
+        mask_img, 
+        interpolation="nearest",
+    )
+    
+    parcel = mask_img.get_fdata().astype(bool)
+    atlas_data = atlas_img.get_fdata().astype(int)
+
+    results = []
+    
+    for region_id, region_name in enumerate(atlas.labels[1:], start=1):
+        region_mask = atlas_data == region_id
+        intersection = np.logical_and(parcel, region_mask).sum()
+
+        results.append({
+            "regions": region_name,
+            "intersection_voxels": intersection,
+            "parcel_overlap": intersection / parcel.sum()
+        })
+    overlap_df = (
+        pd.DataFrame(results)
+        .sort_values("parcel_overlap", ascending=False)
+        .reset_index(drop=True)
+    )
+    return overlap_df[overlap_df["intersection_voxels"] > 0] 
