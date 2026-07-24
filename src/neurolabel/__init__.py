4bf6b4463c32f3312989b9cc98f94b92d8fe90be
mundy-k <63122459+mundy-k@users.noreply.github.com>
2026-07-24 01:31:35 -0700

    Update __init__.py
    Make it so that users can run "from neurolabel import *" and this grabs all current functions (I think)

--- src/neurolabel/__init__.py
+++ src/neurolabel/__init__.py
@@ -1,9 +1,19 @@
-from .functional_decoder import decode_function
-from .utils import extract_parcel_mask, load_neurosynth
+from .utils import extract_parcel_mask, load_neurosynth, load_nilearn, summarize_statistical_map
+from .atlas import atlas_overlap, plot_anat
+from .functional_decoder import decode_function, plot_decoder
+from .visualization import interactive_parcellation_viewer, show_me_jimothy
 from .workflow import run_workflow
 
 __all__ = [
-    "decode_function",
     "extract_parcel_mask",
     "load_neurosynth",
+    "load_nilearn",
+    "summarize_statistical_map",
+    "atlas_overlap",
+    "plot_anat",
+    "decode_function",
+    "plot_decoder",
+    "interactive_parcellation_viewer",
+    "show_me_jimothy",
+    "run_workflow",
 ]
